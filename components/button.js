ec32000ecf614c61a0b1bbf6d02b4e251298ab8d
Joshuamjv2 <joshuamjv22@gmail.com>
2023-06-15 02:22:22 +0300

    article details page mockup

--- components/button.js
+++ components/button.js
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default function Button({text, fa_icon}){
+export default function Button({text, fa_icon, color="#ffc300"}){
     return(
         <button className="">
-        <div className={`text-[#000] cursor-pointer text-center bg-[#ffc300] py-2 rounded-md hover:bg-[#fff] active:bg-[#fcc300] px-6`}>
+        <div className={`text-[#000] cursor-pointer text-center bg-[${color}] py-2 rounded-md hover:bg-[#fff] active:bg-[#fcc300] px-6`}>
             <div className="flex items-center justify-center gap-2">
                 {fa_icon && <FontAwesomeIcon icon={["fas", fa_icon]} size="12" />}
                 <h3 className="capitalize">{text}</h3>
